ffc9ce026737c206d494eeeb053ee2866afe03eb
lythesia <iranaikimi@gmail.com>
2026-07-27 12:01:15 +0800

    player spawn
    1. set phantom camera dynamically coz player spawn is dynamic
    2. rmb call_deferred with `.teleport_position`

---
+++ levels/helper/player_spawn.gd
@@ -0,0 +1,21 @@
+@icon("res://public/icons/player_spawn.svg")
+class_name PlayerSpawn extends Node2D
+
+const PLAYER_SCENE: PackedScene = preload("uid://dgj4nm6qm1ggp")
+
+func _ready() -> void:
+    visible = false
+    await get_tree().process_frame
+
+    # check if already exists a player
+    if get_tree().get_first_node_in_group("Player"):
+        print("Player already exists, skipping spawn")
+        return
+
+    # instantiate player
+    print("Spawning player")
+    var player: Player = PLAYER_SCENE.instantiate()
+    get_tree().current_scene.add_child(player) # player will reparent self in its `_ready()`
+
+    # position player
+    player.position = self.global_position
---
+++ levels/scripts/level_base.gd
@@ -0,0 +1,60 @@
+@tool
+class_name Level extends Node2D
+
+func _ready() -> void:
+    # enable y-sort
+    y_sort_enabled = true
+
+    if Engine.is_editor_hint():
+        return
+
+    # wait player
+    var player: Player = null
+
+    while player == null:
+        player = PlayerManager.get_player()
+        await get_tree().process_frame
+
+    # grab base tilemap layer
+    var base_tilemap_layer: TileMapLayer = _get_base_tilemap_layer()
+    base_tilemap_layer.get_path()
+
+    # create player camera
+    var player_camera: Node = _create_player_camera(player, base_tilemap_layer)
+    add_child(player_camera)
+
+func _get_configuration_warnings() -> PackedStringArray:
+    if !_get_base_tilemap_layer():
+        return ["Requires base tilemap layer named 'Base*'!"]
+    return []
+
+## the most bottom layer of map named "Base*", which defines the boundary/limit of level
+func _get_base_tilemap_layer() -> TileMapLayer:
+    for c in find_children("Base*", "TileMapLayer"):
+        return c as TileMapLayer
+    return null
+
+# Cam
+#  +-- Camera2D
+#  |   +-- PhantomCameraHost
+#  +-- PhantomCamera2D
+func _create_player_camera(player: Player, base_tilemap_layer: TileMapLayer) -> Node:
+    var container: Node = Node.new()
+    container.name = "Cam"
+
+    var base_cam: Camera2D = Camera2D.new()
+    base_cam.name = "Camera2D"
+    var phantom_cam_host: PhantomCameraHost = PhantomCameraHost.new()
+    phantom_cam_host.name = "PhantomCameraHost"
+    base_cam.add_child(phantom_cam_host)
+    container.add_child(base_cam)
+
+    var cam: PhantomCamera2D = PhantomCamera2D.new()
+    cam.name = "PhantomCamera2D"
+    cam.follow_mode = PhantomCamera2D.FollowMode.SIMPLE
+    cam.set_follow_target(player)
+    cam.set_limit_target(base_tilemap_layer.get_path())
+    cam.teleport_position.call_deferred()
+    container.add_child(cam)
+
+    return container
